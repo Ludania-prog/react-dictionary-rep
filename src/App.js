def797a7fe5d60c1ce93694bc3c8f4c202f2a3ae
Ludania-prog <ludania.pris@gmail.com>
2026-08-22 23:50:57 +0100

    Search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Hello world</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="text-center App-footer">
+          Coded by Ludania David hosted on Neftlify
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
