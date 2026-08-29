19a281c36b5b47b8adadfbe083b94b1583d30bb5
Ludania-prog <ludania.pris@gmail.com>
2026-08-30 00:01:35 +0100

    added a phonetic link

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -19,8 +19,7 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-
+    let apiUrl = `/api/v2/entries/en/${keyword}`;
     console.log(apiUrl);
 
     axios
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,5 +2,13 @@ import React from "react";
 
 export default function Phonetic(props) {
   console.log(props.phonetic);
-  return "hello";
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank">
+        Listen
+      </a>
+      <br />
+      {props.phonetic.text}
+    </div>
+  );
 }
