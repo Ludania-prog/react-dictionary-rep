671275ff6848d9618de1ddda2f1b153fcaaca111
Ludania-prog <ludania.pris@gmail.com>
2026-08-29 01:48:07 +0100

    first commit phonetic

--- src/Result.js
+++ src/Result.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Phonetic from "./Phonetic";
 import Meaning from "./Meaning";
 
 export default function Result(props) {
@@ -11,6 +12,14 @@ export default function Result(props) {
   return (
     <div className="Results">
       <h2>{props.results.word}</h2>
+
+      {props.results.phonetics.map(function (phonetic, index) {
+        return (
+          <div key={index}>
+            <Phonetic phonetic={phonetic} />
+          </div>
+        );
+      })}
 
       {props.results.meanings.map(function (meaning, index) {
         return (
